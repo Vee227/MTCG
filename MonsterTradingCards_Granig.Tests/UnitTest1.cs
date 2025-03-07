4fde2631c982e4fd606585b09ce6c293ee7ef6e6
Vee227 <157512338+Vee227@users.noreply.github.com>
2025-03-07 20:11:13 +0100

    vs Dateien nochmal nachpushen

---
+++ MonsterTradingCards_Granig.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MonsterTradingCards_Granig.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
